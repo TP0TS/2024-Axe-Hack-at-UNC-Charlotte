05fd7e7ba15acf77dc09bc0f1a98cba6248d2f9b
tp0t <thomas.walker.powell@gmail.com>
2024-03-16 12:58:21 -0400

    CS FIX

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Swipify:
         self.app.secret_key = "random"
         self.sp_oauth = oauth2.SpotifyOAuth(
             'a8583822cef2478ca27448f3882066c3', 
-            '7365a5b6693a499db6bf445cd34ca2a', 
+            '7365a5b6693a499db6bf445cd34ca2a3', 
             'http://localhost:5000/callback', 
             scope='playlist-modify-private user-top-read'
         )
@@ -57,25 +57,25 @@ class Swipify:
         # Get recommended tracks based on the seed tracks
         recommended_tracks = sp.recommendations(seed_tracks=seed_tracks, limit=1)
 
-        # Extract relevant information for the recommended track
-        recommended_song = recommended_tracks['tracks'][0]
-        song_name = recommended_song['name']
-        artist_name = recommended_song['artists'][0]['name']
-        album_name = recommended_song['album']['name']
-        song_image = recommended_song['album']['images'][0]['url']
-        artist_image = sp.artist(recommended_song['artists'][0]['id'])['images'][0]['url']
-        song_uri = recommended_song['uri']
+        # Extract relevant information for each recommended track
+        recommended_songs = []
+        for track in recommended_tracks['tracks']:
+            song_name = track['name']
+            artist_name = track['artists'][0]['name']
+            album_name = track['album']['name']
+            song_image = track['album']['images'][0]['url']
+            artist_image = sp.artist(track['artists'][0]['id'])['images'][0]['url']
+            song_uri = track['uri']
+            recommended_songs.append({
+                'song_name': song_name,
+                'artist_name': artist_name,
+                'album_name': album_name,
+                'song_image': song_image,
+                'artist_image': artist_image,
+                'song_uri': song_uri
+            })
 
-        # Get the Spotify embed code for the recommended song with autoplay
-        embed_code = f'<iframe src="https://open.spotify.com/embed/track/{song_uri.split(":")[-1]}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media" autoplay="1"></iframe>'
-
-        return render_template('recommended_song_embed.html', 
-                            song_name=song_name, 
-                            artist_name=artist_name, 
-                            album_name=album_name, 
-                            song_image=song_image, 
-                            artist_image=artist_image, 
-                            embed_code=embed_code)
+        return render_template('recommended_songs.html', recommended_songs=recommended_songs)
 
     def add_song_to_playlist(self, playlist_id, song_uri):
         access_token = session.get('access_token')
@@ -85,7 +85,7 @@ class Swipify:
     def run(self):
         self.app.add_url_rule('/', 'index', self.index)
         self.app.add_url_rule('/login', 'login', self.login)
-        self.app.add_url_rule('/callback', 'callback', self.callback)
+        self.app.add_url_rule('/callback', 'callback', self.callback)  # Register the callback route here
         self.app.add_url_rule('/create_playlist', 'create_playlist', self.create_playlist, methods=['GET', 'POST'])
         self.app.add_url_rule('/add_song','add_song_to_playlist', self.add_song_to_playlist, methods=['GET', 'POST'])
         self.app.add_url_rule('/recommended_songs', 'recommended_songs', self.recommended_songs)
